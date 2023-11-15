3fbcca9b6c55792ce67baa47046f4a9822a4c533
DmitryKosolobov <kosolobov.2003@gmail.com>
2023-11-15 22:14:21 +0300

    пробую модальное окно сделать

--- navigation/screens/statistic1.js
+++ navigation/screens/statistic1.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
 import {
     StyleSheet,
     Text,
@@ -6,23 +7,58 @@ import {
     Image,
     Button,
     TouchableOpacity,
+    Modal
   } from "react-native";
 
-  export default function Statistic1() { 
-    return(
-        <SafeAreaView style = {styles.example}>
-            <Text>Statistic1</Text>
-        </SafeAreaView >
-    );
+export default function Statistic1() { 
+  const [infoWindow,setInfoWindow] = useState(false);
 
+  return(
+    <SafeAreaView style = {styles.container}>
+      <StatusBar style="auto" />
+
+      <SafeAreaView>
+        <Text style={styles.title}>Статистика</Text>
+      </SafeAreaView>
+
+      <SafeAreaView>
+
+        <Modal transparent={true} visible={infoWindow} onRequestClose={() => setInfoWindow(false)}>
+          <TouchableOpacity onPressOut={ ()=>setInfoWindow(false)}>
+
+            <TouchableOpacity activeOpacity={1}>
+              <SafeAreaView style={styles.list}>
+                <Text>Инфа</Text>
+              </SafeAreaView>
+            </TouchableOpacity>
+
+          </TouchableOpacity>
+        </Modal>
+        
+        <TouchableOpacity onPress={ ()=>setInfoWindow(true) }>
+          <Image style={{width:48,height:48}} source={require('/Users/dmitriykosolobov/VSCodeProjects/Project/assets/favicon.png')}/>
+        </TouchableOpacity>  
+        
+      </SafeAreaView>
+
+
+
+      {/* <Text>Statistic1</Text> */}
+    </SafeAreaView >
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  list:{
+    backgroundColor: 'red',
+    alignItems: 'center',
+    //position: 'absolute',
+    //height: '100%', width: '100%',
   }
-
-  const styles = StyleSheet.create({
-
-    example : {
-      flex : 1,
-      alignItems : 'center',
-      justifyContent : 'center'
-    }
-     
-  })
+});
